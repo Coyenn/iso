69057ba8a2fe37d1f7df376c5f2957a68abd27f9
Tim Ritter <timrittermail@gmail.com>
2025-07-15 20:23:28 +0200

    Add failsafe

--- src/server/actions/stylesheet/get-custom-stylesheet.ts
+++ src/server/actions/stylesheet/get-custom-stylesheet.ts
@@ -3,11 +3,17 @@
 import { customStylesheetLocation } from "@/src/config/stylesheet";
 
 export async function getCustomStylesheet() {
-	const file = Bun.file(customStylesheetLocation);
+	try {
+		const file = Bun.file(customStylesheetLocation);
 
-	if (!file.exists()) {
-		await Bun.write(customStylesheetLocation, "");
+		if (!file.exists()) {
+			await Bun.write(customStylesheetLocation, "");
+		}
+
+		return await file.text();
+	} catch (error) {
+		console.error(error);
+
+		return "";
 	}
-
-	return await file.text();
 }
